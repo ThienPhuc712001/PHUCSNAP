22218c74f20ffd91dda7783f93cc4bba0a76ff25
ThienPhuc712001 <nguyenduythienphuc71@gmail.com>
2023-09-20 20:43:36 +0700

    VD bai` 7
    VD bai` 7

---
+++ VD bai`7/VD bai` 7.c
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 9/20/2023.
+//
+#include "stdio.h"
+
+int d2_chanle(){}
+int main(){
+    int num,res ;
+    printf("Enter the number:");
+    scanf("%d,&num");
+    res = num %2;
+    printf(res==0 ? "The number is even.\n" : "The number is odd.\n");
+    return 0;
+}
